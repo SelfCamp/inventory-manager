e6d5340cb0f09d9fdc2d7b79e2f193db7f2e549c
Adam Hosman <github@adamsinbox.com>
2018-12-04 00:47:39 +0100

    Implement basic functionality for menu items 1-3

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cnx
-import test_queries
 import midrate
+import test_queries
+import ui
 
 
 @cnx.connection_handler
@@ -48,32 +49,71 @@ def reset_database(cursor):
 
 @cnx.connection_handler
 def get_inventory(cursor):
-    print('→ This will check complete inventory when the feature is implemented')
-    input('Press [Enter] to return to MENU')
-    # cursor.execute('SELECT * FROM inventory;') # TODO: JOIN, add products.name
-    # result = cursor.fetchall()
-    # # TODO: finish
+    """Fancy-print complete inventory across all locations"""
+    cursor.execute("""
+        SELECT inv.location_id, inv.quantity, inv.expiration_date, inv.rack_no, inv.shelf_no,
+               prod.name, prod.unit
+        FROM inventory inv JOIN products prod
+        ON inv.product_id = prod.product_id
+        ORDER BY prod.name, inv.location_id, inv.expiration_date;
+    """)
+    result = cursor.fetchall()
+    ui.print_title('Complete inventory across all locations (ordered by item name, location, then expiration date):')
+    for loc, qty, exp, rack, shelf, name, unit in result:
+        print(f'{name}: {qty} {unit} at {loc} on rack {rack}, shelf {shelf} (expires on {exp})')
+    input('\nPress [Enter] to return to MENU')
 
 
 @cnx.connection_handler
 def get_stock_level_for_product_id(cursor):
-    print('→ This will check stock level by product ID when the feature is implemented')
-    input('Press [Enter] to return to MENU')
-    # requested_product_id = input('Please enter requested product ID: ')
-    # cursor.execute(f"""
-    #     SELECT products.product_id, products.name, products.unit, inventory.quantity,  FROM inventory
-    #     JOIN products ON inventory.product_id = products.product_id
-    #     WHERE products.product_id = {requested_product_id}
-    # """)
-    # result = cursor.fetchall()
-    # # TODO: finish
+    """Print stock level for a given product ID from user input"""
+    product_id = input('Please enter product ID: ')
+    cursor.execute(f"""
+        SELECT inv.location_id, inv.quantity, inv.expiration_date, inv.rack_no, inv.shelf_no,
+               prod.name, prod.unit
+        FROM inventory inv JOIN products prod
+        ON inv.product_id = prod.product_id
+        WHERE prod.product_id = {product_id}
+        ORDER BY inv.location_id, inv.expiration_date;;
+    """)
+    result = cursor.fetchall()
+    name = result[0][5]
+    ui.print_title(f'Inventory of \'{name}\' across all locations (ordered by location, then expiration date):')
+    for loc, qty, exp, rack, shelf, name, unit in result:
+        print(f'{qty} {unit} at {loc} on rack {rack}, shelf {shelf} (expires on {exp})')
+    input('\nPress [Enter] to return to MENU')
 
 
 @cnx.connection_handler
 def update_stock_level_for_inventory_id(cursor):
-    print('→ This will update stock level by inventory ID when the feature is implemented')
+    """Update stock level for a given inventory ID from user input"""
+    inventory_id = input('Please enter inventory ID: ')
+    cursor.execute(f"""
+        SELECT inv.location_id, inv.quantity, inv.expiration_date, inv.rack_no, inv.shelf_no,
+               prod.name, prod.unit
+        FROM inventory inv JOIN products prod
+        ON inv.product_id = prod.product_id
+        WHERE inv.inventory_id = {inventory_id}
+    """)
+    result = cursor.fetchall()
+    for loc, qty, exp, rack, shelf, name, unit in result:
+        print(f'\nCurrent quantity of \'{name}\' at {loc} on rack {rack}, shelf {shelf} (expires on {exp}): {qty} {unit}')
+    new_level = input('Please enter new quantity: ')
+    cursor.execute(f"""
+        UPDATE inventory SET quantity = {new_level}
+        WHERE inventory_id = {inventory_id}
+    """)
+    cursor.execute(f"""
+        SELECT inv.location_id, inv.quantity, inv.expiration_date, inv.rack_no, inv.shelf_no,
+               prod.name, prod.unit
+        FROM inventory inv JOIN products prod
+        ON inv.product_id = prod.product_id
+        WHERE inv.inventory_id = {inventory_id}
+    """)
+    result = cursor.fetchall()
+    for loc, qty, exp, rack, shelf, name, unit in result:
+        print(f'\nUpdated quantity of \'{name}\' at {loc} on rack {rack}, shelf {shelf} (expires on {exp}): {qty} {unit}')
     input('Press [Enter] to return to MENU')
-    # TODO: implement
 
 
 def quit_application():
