502c4e0849711710edf433e8ec44c32a1fd52d37
Oliver Juhasz <oliver.juhasz@outlook.com>
2018-12-03 02:55:21 +0100

    Updated midrate fetcher for better performance

--- midrate.py
+++ midrate.py
@@ -9,10 +9,11 @@ import itertools
 @cnx.connection_handler
 def midrate_updater(cursor):
     """ Checks if midrate table is up to date. Updates table from napiarfolyam.hu if dates are in the past."""
+    """
     if is_midrate_up_to_date():
         print("Foreign currency mid-rates are up to date!")
         return None
-
+"""
     response = requests.get("http://api.napiarfolyam.hu/?bank=mnb")
     root = ET.fromstring(response.text)
 
@@ -21,11 +22,11 @@ def midrate_updater(cursor):
     currency_rate_list = list(zip(currency_list, midrate_list))
 
     cursor.execute("DELETE FROM mid_exchange_rate")
-
+    to_sql = "INSERT INTO mid_exchange_rate (currency_id, date_updated, midrate_to_huf) VALUES "
     for elem in currency_rate_list:
-        cursor.execute("INSERT INTO mid_exchange_rate (currency_id, date_updated, midrate_to_huf)"
-                       "VALUES ('{}','{}','{}')".format(elem[0], dt.date.today(), elem[1]))
-
+        to_sql += " ('{}','{}','{}'),".format(elem[0], dt.date.today(), elem[1])
+    to_sql = to_sql[:-1]
+    cursor.execute(to_sql)
 
 @cnx.connection_handler
 def is_midrate_up_to_date(cursor):
@@ -64,7 +65,7 @@ def sql_table_import(cursor, file, database):
 
 
 if __name__ == '__main__':
-    sql_table_import(r"drafts\contacts_table.csv", "contacts")
+    midrate_updater()
 
 #TODO: inventory exp date: show only date, time is not needed
 #TODO: Fix phone number for contacts
