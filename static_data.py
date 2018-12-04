4e601421ecd04a225ad7ccace0a4f4ce8416dd6a
Oliver Juhasz <oliver.juhasz@outlook.com>
2018-12-04 16:44:00 +0100

    Added "static_data" file to store larger, static data, list, dicts
    Added database/file directory dictionary

---
+++ static_data.py
@@ -0,0 +1,14 @@
+database_dict = {"inventory": r"drafts/inventory_table.csv",
+                 "locations": r"drafts/locations_table.csv",
+                 "products": r"drafts/products_table.csv",
+                 "products_to_suppliers": r"drafts/products_to_suppliers_table.csv",
+                 "menu_items": r"drafts/menu_items_table.csv",
+                 "proportions": r"drafts/proportions_table.csv",
+                 "suppliers": r"drafts/suppliers_table.csv",
+                 "contacts": r"drafts/contacts_table.csv",
+                 "users": r"drafts/users_table.csv",
+                 "purchase_orders": r"drafts/purchase_orders_table.csv",
+                 "purchase_order_contents": r"drafts/purchase_order_contents_table.csv",
+                 "access_levels": r"drafts/access_levels_table.csv",
+                 "employees": r"drafts/employees_table.csv"
+                 }
