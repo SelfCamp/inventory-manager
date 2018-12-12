ca9f769436e09a7e5819ae8d6106133904073d83
Oliver Juhasz <oliver.juhasz@outlook.com>
2018-12-12 16:38:52 +0100

    Update User class to handle sql query in INIT

--- classes/User.py
+++ classes/User.py
@@ -1,26 +1,29 @@
+from menu_functions import read_functions as rf
+
 class User:
 
-    def __init__(self, username, access_level, first_name, last_name, location_id, department, role, locality, phone,
-                 email, address_line1, address_line2, region, postcode, country, salary):
-        self.username = username
-        self.access = access_level
-        self.first_name = first_name
-        self.last_name = last_name
-        self.location = location_id
-        self.department = department
-        self.role = role
-        self.locality = locality
-        self.phone = phone
-        self.email = email
-        self.address_line1 = address_line1
-        self.address_line2 = address_line2
-        self.region = region
-        self.postcode = postcode
-        self.country = country
-        self.salary = salary
+    def __init__(self, username):
+        user_data = rf.get_employee_data(username)
+        self.username = user_data["username"]
+        self.access_level_id = user_data["access_level_id"]
+        self.first_name = user_data["first_name"]
+        self.last_name = user_data["last_name"]
+        self.location_id = user_data["location_id"]
+        self.department = user_data["department"]
+        self.role = user_data["role"]
+        self.locality = user_data["locality"]
+        self.phone_no = user_data["phone_no"]
+        self.email = user_data["email"]
+        self.address_line1 = user_data["address_line1"]
+        self.address_line2 = user_data["address_line2"]
+        self.region = user_data["region"]
+        self.postcode = user_data["postcode"]
+        self.country = user_data["country"]
+        self.salary_huf = user_data["salary_huf"]
+
 
     def __repr__(self):
-        return f"Name: {self.last_name}, {self.first_name} \nLocation ID: {self.location} \n" \
+        return f"Name: {self.last_name}, {self.first_name} \nLocation ID: {self.location_id} \n" \
             f"Department: {self.department} Role: {self.role}\n" \
             f"Address: {self.country} {self.postcode} {self.locality} {self.address_line1} {self.address_line2} \n" \
-            f"Contacts: \nPhone: {self.phone}, e-mail: {self.email} "
+            f"Contacts: \nPhone: {self.phone_no}, e-mail: {self.email} "
