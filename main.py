ac05f17e42e11a5214c57c24a592fa2b4e2c5a85
Adam Hosman <github@adamsinbox.com>
2018-12-12 18:13:58 +0100

    Change MENU to generate numbers dynamically

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ from menu_functions import admin_functions as af, read_functions as rf, update_f
 
 def menu_handler(current_user):
     MENU = [
-        {'description': '0: Reset database',                      'fn': af.reset_database},
-        {'description': '1: Check complete inventory',            'fn': rf.get_inventory},
-        {'description': '2: Check stock level for product ID',    'fn': rf.get_stock_level_for_product_id},
-        {'description': '3: Request supplier information',        'fn': rf.get_available_suppliers},
-        {'description': '4: Check status of purchase order',      'fn': rf.get_po_status_for_po_id},
-        {'description': '5: Update stock level for inventory ID', 'fn': uf.set_stock_level_for_inventory_id},
-        {'description': '6: Quit application',                    'fn': quit_application}
+        {'description': 'Reset database',                      'fn': af.reset_database},
+        {'description': 'Check complete inventory',            'fn': rf.get_inventory},
+        {'description': 'Check stock level for product ID',    'fn': rf.get_stock_level_for_product_id},
+        {'description': 'Request supplier information',        'fn': rf.get_available_suppliers},
+        {'description': 'Check status of purchase order',      'fn': rf.get_po_status_for_po_id},
+        {'description': 'Update stock level for inventory ID', 'fn': uf.set_stock_level_for_inventory_id},
+        {'description': 'Quit application',                    'fn': quit_application}
     ]
     print('\nMENU')
-    for item in MENU:
-        print(item['description'])
+    for num, item in enumerate(MENU):
+        print(f"{num}: {item['description']}")
     choice = int(input('\nPlease type number of your choice: '))
     MENU[choice]['fn'](current_user)
     input('\nPress [Enter] to return to MENU')
