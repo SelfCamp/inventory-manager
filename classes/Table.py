22ddd389e1d3616aa9366e58b4d97b28a3171efc
Adam Hosman <github@adamsinbox.com>
2018-12-14 20:12:55 +0100

    Tweak Table class
    - PEP8
    - interface uniformity
      - remove starred expression from `*params` (to make `__init__()` take same arguments as mysql-connector’s `cursor.execute()`)
    - make headers a tuple to match rows

--- classes/Table.py
+++ classes/Table.py
@@ -1,18 +1,18 @@
 from common import cnx
 
+
 class Table:
 
-
-    def __init__(self, sql_read_statement, *params):
-        self.headers, self.data = self._connection(sql_read_statement, *params)
-        self.table = tuple([self.headers,]) + tuple(self.data)
+    def __init__(self, sql_read_statement, params):
+        self.headers, self.data = self._connection(sql_read_statement, params)
+        self.table = tuple([self.headers]) + tuple(self.data)
 
     @staticmethod
     @cnx.connection_handler()
-    def _connection(cursor, statement, *params):
-        cursor.execute(statement, *params)
+    def _connection(cursor, statement, params):
+        cursor.execute(statement, params)
         data = cursor.fetchall()
-        return [i[0] for i in cursor.description], data
+        return tuple(i[0] for i in cursor.description), data
 
     def __repr__(self):
         column_lengths = [max([len(str(i[x])) for i in self.table]) for x in range(len(self.data[0]))]
@@ -24,7 +24,7 @@ class Table:
             header_text += "|" + " " * padding_left + str(header) + " " * padding_right
             divider_text += "-" * (padding_left + padding_right + len(header)) + "+"
         header_text += "|"
-        full_text = divider_text + "\n" +  header_text + "\n" + divider_text + "\n"
+        full_text = divider_text + "\n" + header_text + "\n" + divider_text + "\n"
 
         for record in self.data:
             for i, field in enumerate(record):
@@ -35,7 +35,5 @@ class Table:
         full_text += divider_text
         return full_text
 
-
-
     def sort_by(self, column, asc=False):
-        self.data = sorted(self.data, key= lambda x: x[column], reverse= asc)
+        self.data = sorted(self.data, key=lambda x: x[column], reverse=asc)
